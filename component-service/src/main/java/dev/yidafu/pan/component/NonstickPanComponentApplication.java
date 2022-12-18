ebe2ed2ded2208ee85c769bfe9b1cc9a5374bf34
yidafu <me@yidefu.dev>
2022-12-18 19:14:46 +0800

    refactor: change DTO class name

--- component-service/src/main/java/dev/yidafu/pan/component/NonstickPanComponentApplication.java
+++ component-service/src/main/java/dev/yidafu/pan/component/NonstickPanComponentApplication.java
@@ -4,8 +4,10 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * https://blog.csdn.net/weixin_43118901/article/details/109153215
+ */
 @SpringBootApplication
-@MapperScan("dev.yidafu.pan.component")
 public class NonstickPanComponentApplication {
 
 	public static void main(String[] args) {
